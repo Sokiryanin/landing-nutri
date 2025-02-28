6ce6b86ca29ad7b3125a5c1e81eee80141dd7160
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2025-02-28 15:40:55 +0100

    bug fix

--- src/js/libs/countdown.js
+++ src/js/libs/countdown.js
@@ -19,6 +19,36 @@ function updateCountdown() {
     document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
 }
 
+function getTomorrowDate() {
+  let tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  let day = tomorrow.getDate();
+  let months = {
+    1: 'січня',
+    2: 'лютого',
+    3: 'березня',
+    4: 'квітня',
+    5: 'травня',
+    6: 'червня',
+    7: 'липня',
+    8: 'серпня',
+    9: 'вересня',
+    10: 'жовтня',
+    11: 'листопада',
+    12: 'грудня',
+  };
+
+  let month = months[tomorrow.getMonth() + 1];
+
+  return `${day} ${month}`;
+}
+
+document.getElementById(
+  'start-date'
+).textContent = `Старт ${getTomorrowDate()}`;
+
 // Обновление каждую секунду
 setInterval(updateCountdown, 1000);
 updateCountdown();
+getTomorrowDate();
